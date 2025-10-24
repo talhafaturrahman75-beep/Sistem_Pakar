e1902b01f10da7d7bd8484df7cff0ea9d77ecc01
talhafaturrahman75-beep <talhafaturrahman75@gmail.com>
2025-10-24 17:39:13 +0700

    Memperbaiki agar file engine yang dituju lebih spesifik

--- ui/app.py
+++ ui/app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json, os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'inference_engine'))
-from engine import load_rules, evaluate
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from inference_engine.engine import load_rules, evaluate
 
 app = Flask(__name__)
 
 RULES_PATH = os.path.join(os.path.dirname(__file__), '..', 'rules.json')
 rules = load_rules(RULES_PATH)
 
-# gather unique symptoms
 symptoms = []
 for r in rules:
     for p in r['premises']:
@@ -23,20 +22,17 @@ def index():
 def diagnose():
     user_facts = {}
     
-    # Loop menggunakan enumerate untuk mendapatkan index (0, 1, 2, ...)
     for i, s in enumerate(symptoms):
         sym_key = 'sym_' + str(i)
-        conf_key = 'conf_' + sym_key # Nama dropdown adalah 'conf_sym_0', 'conf_sym_1', dst.
+        conf_key = 'conf_' + sym_key
         
-        # Cek HANYA jika checkbox dicentang
         if sym_key in request.form:
-            # Ambil nilai dari dropdown yang sesuai
             confidence_str = request.form.get(conf_key, '0.5') # Default ke 0.5 (Mungkin)
             
             try:
                 val = float(confidence_str)
             except ValueError:
-                val = 0.5 # Fallback
+                val = 0.5
                 
             user_facts[s] = val
 
